e19bd6327c725498e632499cfaf5c5f6c410fd09
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-05-05 18:12:03 -0400

    commit tree 5ca7d0455f6d25eadc21059b83748b0797a1ea0b

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  // GET -------------------------------------------
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => setQuestions(questions));
+  }, []);
+
+// DELETE -------------------------------------------
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        const updatedQuestionList = questions.filter((question) => question.id !== id);
+        setQuestions(updatedQuestionList);
+        });
+  }
+
+  // PATCH -------------------------------------------
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((response) => response.json())
+      .then((changeCorrectAnswer) => {
+        const updatedQuestionList = questions.map((question) => {
+          if (question.id ===  changeCorrectAnswer.id) {
+            return changeCorrectAnswer;
+          }
+            return question
+        });
+        setQuestions(updatedQuestionList);
+        });
+  }
+
+  const questionList = questions.map((question) => (
+    <QuestionItem 
+    key={question.id}
+    question={question}
+    handleDelete={handleDelete}
+    handleAnswerChange={handleAnswerChange}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionList}</ul>
     </section>
   );
 }
